d8e0f00637dbb9ec10519453fa1ea6c8d1dd4945
salas89 <miguel.sap89@gmail.com>
2023-12-31 10:24:05 +0100

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -5,12 +5,12 @@ palabra2 = "amor"
 def anagrama (a:str,b:str):
     a=a.lower()
     b=b.lower()
-    c=sorted(a)== sorted(b)
+    c=sorted(a)== sorted(b) #sorted ordena los elementos de la variable, lista, etc.
 
     if a==b:
         return ("No es un anagrama, son la misma palabra")
     elif c==True:
-        return ("Es un anagrama") #sorted ordena los elementos de la variable, lista, etc.
+        return ("Es un anagrama") 
     else: return("No es un anagrama")
 
 print(anagrama(palabra1,palabra2))
