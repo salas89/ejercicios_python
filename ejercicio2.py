b8b656bd0b197c8f8a48365c29d5cc2ab74d384d
salas89 <miguel.sap89@gmail.com>
2023-12-31 10:06:24 +0100

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -8,6 +8,6 @@ def anagrama (a:str,b:str):
 
     if a==b:
         print ("No es un anagrama")
-    elif a!=b: sorted(a)== sorted(b)
+    elif a!=b: sorted(a)== sorted(b) #sorted ordena los elementos de la variable, lista, etc.
 
 print(anagrama(palabra1,palabra2))
