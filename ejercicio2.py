659f11eb26b8f0632585a481fd470591964bcccf
salas89 <miguel.sap89@gmail.com>
2023-12-31 10:04:54 +0100

    Update ejercicio2.py

--- ejercicio2.py
+++ ejercicio2.py
@@ -1,1 +1,13 @@
 
+palabra1= "amoR"
+palabra2 = "amor"
+
+def anagrama (a:str,b:str):
+    a=a.lower()
+    b=b.lower()
+
+    if a==b:
+        print ("No es un anagrama")
+    elif a!=b: sorted(a)== sorted(b)
+
+print(anagrama(palabra1,palabra2))
